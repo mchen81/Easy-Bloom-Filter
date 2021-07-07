68e4cfb552b77bd5cf2087be079fb4e6b4ccc211
Jerry <mchen81@dons.usfca.edu>
2021-07-07 10:49:12 +0800

    finished basic functionality

---
+++ go.mod
@@ -0,0 +1,8 @@
+module bloomfilter
+
+go 1.16
+
+require (
+	github.com/bits-and-blooms/bitset v1.2.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bits-and-blooms/bitset"
+	"github.com/spaolacci/murmur3"
+)
+
+type BloomFilter struct {
+	filters int
+	hashs   int
+	bs      *bitset.BitSet
+	seed    int
+	count   int
+}
+
+func NewBloomFilter() *BloomFilter {
+	var b bitset.BitSet
+	return &BloomFilter{10000, 3, &b, 3, 0}
+}
+
+func (bf *BloomFilter) put(data []byte) {
+	bf.count++
+	for i := 0; i < bf.seed; i++ {
+		p := murmur3.Sum32(data) % uint32(bf.filters)
+		bf.bs.Set(uint(p))
+	}
+}
+
+func (bf *BloomFilter) check(data []byte) bool {
+	contain := true
+	for i := 0; i < bf.seed; i++ {
+		p := murmur3.Sum32(data) % uint32(bf.filters)
+		contain = contain && bf.bs.Test(uint(p))
+	}
+	return contain
+}
+
+func main() {
+	bf := NewBloomFilter()
+
+	s1 := "test1"
+	s2 := "test2"
+	s3 := "test3"
+	s4 := "test4"
+
+	bf.put([]byte(s1))
+	bf.put([]byte(s2))
+	bf.put([]byte(s3))
+	bf.put([]byte(s4))
+
+	fmt.Println(bf.check([]byte(s1)))
+	fmt.Println(bf.check([]byte(s2)))
+	fmt.Println(bf.check([]byte(s3)))
+	fmt.Println(bf.check([]byte(s4)))
+
+}
