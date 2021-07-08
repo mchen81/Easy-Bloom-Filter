8d55f7bb6fe27124b9d43b50d873176313e33060
Jerry <mchen81@dons.usfca.edu>
2021-07-08 21:08:29 +0800

    optimize code

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module bloomfilter
+module "gitgub.com/mchen81/easy-bloom-filter"
 
 go 1.16
 
 require (
-	github.com/bits-and-blooms/bitset v1.2.0 // indirect
-	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/bits-and-blooms/bitset v1.2.0
+	github.com/spaolacci/murmur3 v1.1.0
 )
--- main.go
+++ main.go
@@ -1,17 +1,15 @@
-package main
+package bloomfilter
 
 import (
-	"fmt"
-
 	"github.com/bits-and-blooms/bitset"
 	"github.com/spaolacci/murmur3"
 )
 
 type BloomFilter struct {
-	filters int
+	filters uint32
 	hashs   uint32
 	bs      *bitset.BitSet
-	count   int
+	count   uint
 }
 
 func NewBloomFilter() *BloomFilter {
@@ -23,37 +21,17 @@ func (bf *BloomFilter) put(data []byte) {
 	bf.count++
 	var seed uint32
 	for seed = 0; seed < bf.hashs; seed++ {
-		p := murmur3.Sum32WithSeed(data, seed) % uint32(bf.filters)
+		p := murmur3.Sum32WithSeed(data, seed) % bf.filters
 		bf.bs.Set(uint(p))
 	}
 }
 
 func (bf *BloomFilter) check(data []byte) bool {
-	contain := true
+	isContain := true
 	var seed uint32
 	for seed = 0; seed < bf.hashs; seed++ {
-		p := murmur3.Sum32WithSeed(data, seed) % uint32(bf.filters)
-		contain = contain && bf.bs.Test(uint(p))
+		p := murmur3.Sum32WithSeed(data, seed) % bf.filters
+		isContain = isContain && bf.bs.Test(uint(p))
 	}
-	return contain
-}
-
-func main() {
-	bf := NewBloomFilter()
-
-	s1 := "test1"
-	s2 := "test2"
-	s3 := "test3"
-	s4 := "test4"
-
-	bf.put([]byte(s1))
-	bf.put([]byte(s2))
-	bf.put([]byte(s3))
-	bf.put([]byte(s4))
-
-	fmt.Println(bf.check([]byte(s1)))
-	fmt.Println(bf.check([]byte(s2)))
-	fmt.Println(bf.check([]byte(s3)))
-	fmt.Println(bf.check([]byte(s4)))
-
+	return isContain
 }
